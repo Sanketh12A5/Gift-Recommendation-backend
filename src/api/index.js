96b10d1597ea3fd88e7b56c32fb1c70833689209
Dhanpal Sanketh <132679523+Sanketh12A5@users.noreply.github.com>
2025-04-07 20:56:50 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,13 @@ const app = express();
 //const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+      origin : {"https://localhost:5173/"},
+      methods : {"POST", "GET"},
+      credentials : true
+  }
+));
 app.use(express.json());
 
 // Routes
